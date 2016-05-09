3222f175a048645741c65193ed2a6456b4694ac3
sungjin0426 <sjlbest0426@gmail.com>
2016-05-09 11:35:59 -0400

    heroku frontend

--- frontend/app/utils/ajaxHelpers.js
+++ frontend/app/utils/ajaxHelpers.js
@@ -6,15 +6,15 @@ const ajaxHelpers = {
   },
 
   getUserAgenda: function(){
-    return axios.get('http://localhost:3000/photos')
+    return axios.get('http://hidden-agenda-rails-backend.herokuapp.com/photos')
   },
 
   getMapResults: function(lat, lng){
-      return axios.get('https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&api_key=084f83671fd818ada2b7545f262bc64c&lat=' + lat + '&lon=' + lng + '&format=json&nojsoncallback=1');
+      return axios.get('https://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&api_key=021e9c0509d04ce2b687da4affd991d6&lat=' + lat + '&lon=' + lng + '&format=json&nojsoncallback=1');
   },
 
   addAgenda: function(photo_rails){
-    return axios.post('http://localhost:3000/photos', photo_rails);
+    return axios.post('http://hidden-agenda-rails-backend.herokuapp.com/photos', photo_rails);
   },
 
 }
