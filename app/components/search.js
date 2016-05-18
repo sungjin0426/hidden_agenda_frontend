68d42ca6dd047a5509f0aa60606beebc1bbb174e
sungjin0426 <sjlbest0426@gmail.com>
2016-05-18 16:01:16 -0400

    styling

--- app/components/search.js
+++ app/components/search.js
@@ -3,12 +3,15 @@ import {Button, Card, Row, Col} from 'react-materialize';
 
 
 const SearchName = React.createClass({
-
   render: function(){
+    const inputstyle = {
+      fontSize: '30px'
+    }
+    
     return(
       <div className='searchbyname'>
         <h3>Search by Name:</h3>
-        <input onChange={this.props.onChangeName} value={this.props.markerCityName} type='text' placeholder='search by name' />
+        <input style={inputstyle} onChange={this.props.onChangeName} value={this.props.markerCityName} type='text' placeholder='search by name' />
         <Button waves='light' className="Submit" onClick={this.props.onSubmit} type='button'>Submit!</Button>
 
 
