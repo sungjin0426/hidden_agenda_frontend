547ee269d81d3468a8250b80da7646b6b7a84b62
sungjin0426 <sjlbest0426@gmail.com>
2016-05-19 15:51:18 -0400

    final

--- app/components/About.js
+++ app/components/About.js
@@ -10,7 +10,7 @@ const About = React.createClass({
       <div className= "aboutus">
       <br/><br/><br/>
         <p>We wanted to make an application that will allow users to save their favorite travel destination photos and locations. The user has the option to manually enter a city, or they can "explore" the map and drag the marker which will populate the input box with the city.</p>
-        <p>Instagram and twitter are great ways of finding new places but not a great way of saving information. So by using the flickr api and mapbox api, we were able to append photos by search and then by using ruby on rails we were able to save it on our backend and reappend it to the saved page. We also incorporated user authorization so users can have their own account on the app and go back to their saved travel photos.
+        <p>Using the flickr api and mapbox api, we were able to append photos by search and then by using ruby on rails we were able to save it on our backend and reappend it to the saved page. We also incorporated user authorization so users can have their own account on the app and go back to their saved travel photos.
         </p>
         <h4>Created by</h4>
         <ul style={ulStyle}>
